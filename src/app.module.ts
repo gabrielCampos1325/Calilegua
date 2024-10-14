220660c6ed6fbdd4dd447c70e37945a3c2d2df54
Gabriel Alberto Campos <camposgabriel372@gmail.com>
2024-10-14 15:27:23 -0300

    modifico el operadores.module y productos.module para que cada uno tenga sus controllers y providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,40 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { FabricantesController } from './productos/controllers/fabricantes.controller';
-import { ProductosController } from './productos/controllers/productos.controller';
-import { PedidosController } from './operadores/controllers/pedidos.controller';
-import { OperadoresController } from './operadores/controllers/operadores.controller';
-import { CompradoresController } from './operadores/controllers/compradores.controller';
-import { CategoriasController } from './productos/controllers/categorias.controller';
-import { ProductosService } from './productos/services/productos.service';
-import { PedidosService } from './operadores/services/pedidos.service';
-import { OperadoresService } from './operadores/services/operadores.service';
-import { FabricantesService } from './productos/services/fabricantes.service';
-import { CompradoresService } from './operadores/services/compradores.service';
-import { CategoriasService } from './productos/services/categorias.service';
 import { OperadoresModule } from './operadores/operadores.module';
 import { ProductosModule } from './productos/productos.module';
 
 @Module({
   imports: [OperadoresModule, ProductosModule],
-  controllers: [
-    AppController,
-    FabricantesController,
-    ProductosController,
-    PedidosController,
-    OperadoresController,
-    CompradoresController,
-    CategoriasController,
-  ],
-  providers: [
-    AppService,
-    ProductosService,
-    PedidosService,
-    OperadoresService,
-    FabricantesService,
-    CompradoresService,
-    CategoriasService,
-  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/operadores/operadores.module.ts
+++ src/operadores/operadores.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { CompradoresController } from './controllers/compradores.controller';
+import { OperadoresController } from './controllers/operadores.controller';
+import { PedidosController } from './controllers/pedidos.controller';
+import { CompradoresService } from './services/compradores.service';
+import { OperadoresService } from './services/operadores.service';
+import { PedidosService } from './services/pedidos.service';
 
-@Module({})
+@Module({
+  controllers: [PedidosController, OperadoresController, CompradoresController],
+  providers: [PedidosService, OperadoresService, CompradoresService],
+})
 export class OperadoresModule {}
