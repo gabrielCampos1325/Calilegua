607dd23f095ca4fb1d9d3e3568865d7cfd9fb711
Gabriel Alberto Campos <camposgabriel372@gmail.com>
2024-10-21 07:35:46 -0300

    Agrego modulo global Database e implemento el useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,25 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { lastValueFrom } from 'rxjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OperadoresModule } from './operadores/operadores.module';
 import { ProductosModule } from './productos/productos.module';
-
-const APIKEY = 'DEV-456';
-const APIKEYPROD = 'PROD-12345';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [OperadoresModule, ProductosModule],
+  imports: [HttpModule, OperadoresModule, ProductosModule, DatabaseModule],
   controllers: [AppController],
   providers: [
     AppService,
     {
-      provide: 'APIKEY',
-      useValue: process.env.NODE_ENV === 'prod' ? APIKEYPROD : APIKEY,
+      provide: 'TAREA_ASINC',
+      useFactory: async (http: HttpService) => {
+        const req = http.get('https://jsonplaceholder.typicode.com/posts');
+        const tarea = await lastValueFrom(req);
+        return tarea.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
